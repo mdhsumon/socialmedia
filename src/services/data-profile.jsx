2b67e63e0e9e07fb5cfbb1be6628b21c966c2c50
delower <delower@bitmascot.com>
2019-02-03 17:23:56 +0600

    component creation

---
+++ src/services/data-profile.jsx
@@ -0,0 +1,14 @@
+export const DataProfile = {   id: 1, 
+    userName: "kuddus-boyati",
+    Name: "Kuddus - Boyati",
+    nickName: "Boyati",
+    profilePhoto: "profile-photo.jpg",
+    coverPhoto: "cover-photo.jpg",
+    profileMenu: [
+        { slug: "timeline", name: "Timeline" },
+        { slug: "about", name: "About" },
+        { slug: "friends", name: "Friends" },
+        { slug: "photos", name: "Photos" },
+        { slug: "videos", name: "Videos" }
+    ]
+}
