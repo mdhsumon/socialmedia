d6810aff4e943dd6315e945ee5927230bc055e53
sumon <mdhsumon@gmail.com>
2019-09-25 23:00:12 +0600

    friend request

--- src/components/global/FriendRequests.jsx
+++ src/components/global/FriendRequests.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { getFriendRequests, getMultipleUserSummary } from "../../services/userService";
+import { Link } from "react-router-dom";
 
 export default class FriendRequests extends React.Component {
     constructor(props) {
@@ -10,15 +11,19 @@ export default class FriendRequests extends React.Component {
     }
 
     componentDidMount() {
-        this.FriendRequests();
+        this.loadFriendRequests();
     }
 
-    FriendRequests = () => {
+    loadFriendRequests = () => {
         const userData = localStorage.getItem("userData") !== null ? JSON.parse(localStorage.getItem("userData")) : "";
         getFriendRequests(userData.userInfo.username, response => {
-            //this.setState({friendRequests: response.friendRequests})
-            getMultipleUserSummary();
-            console.log(response)
+            let reqSenderId = [];
+            for(let sender in response.friendRequests) {
+                reqSenderId[sender] = response.friendRequests[sender].senderId;
+            }
+            getMultipleUserSummary(reqSenderId, response => {
+                this.setState({friendRequests: response})
+            });
         })
     }
 
@@ -38,18 +43,18 @@ export default class FriendRequests extends React.Component {
             <div className="common-block friend-request-section">
                 <h4 className="block-header">Friend requests</h4>
                 <div className="block-body friend-rq-items">
-                    {this.state.friendRequests.map((requestItem, key) => {
+                    {this.state.friendRequests.map(requestItem => {
                         return(
-                            <div className="friend-rq-item" key={requestItem.senderId}>
+                            <div className="friend-rq-item" key={requestItem.userId}>
                                 <div className="friend-rq-item-thumb">
-                                    <img alt="alter text" src="http://localhost:2000/file/global/image/female.png" />
+                                    <img alt="alter text" src={requestItem.profilePhoto} />
                                 </div>
                                 <div className="friend-rq-item-content">
-                                    <a className="author-name" href="/">komola banu</a>
+                                    <Link className="author-name" to={`/${requestItem.username}`}>{requestItem.displayName}</Link>
                                     <p className="mutual-f"><span className="mutual-amount">5</span> mutual friends</p>
                                     <div className="rq-buttons">
-                                        <button className="accept" onClick={this.handleRequest.bind(this, requestItem.senderId)}>Accept</button>
-                                        <button className="decline" onClick={this.handleRequest.bind(this, requestItem.senderId)}>Decline</button>
+                                        <button className="accept" onClick={this.handleRequest.bind(this, requestItem.userId)}>Accept</button>
+                                        <button className="decline" onClick={this.handleRequest.bind(this, requestItem.userId)}>Decline</button>
                                     </div>
                                 </div>
                             </div>
