3abbfde7000566f856bb4fb5be9db63b166e1fea
Delower Hossain <delower.office@gmail.com>
2019-10-05 23:14:15 +0600

    chat ui

--- src/components/chat/ChatBar.jsx
+++ src/components/chat/ChatBar.jsx
@@ -1,12 +1,32 @@
 import React from "react"
 import { apiBaseUrl } from "../../services/commonService"
+import { getFriendLists, getMultipleUserSummary } from "../../services/userService"
 
 export class ChatBar extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            chatUsers: [1,2,3,4,5]
+            userStatus: 'offline',
+            chatUsers: []
         }
+    }
+
+    componentDidMount() {
+        getFriendLists(response => {
+            let friendIds = []
+            for(let friend in response.friends) {
+                friendIds[friend] = response.friends[friend].friendId
+            }
+            getMultipleUserSummary(friendIds, users => {
+                this.setState({
+                    chatUsers: users
+                })
+            })
+        })
+    }
+
+    onlineStatus = () => {
+
     }
 
     render() {
@@ -16,9 +36,10 @@ export class ChatBar extends React.Component {
                     <div className="user-list">
                         {this.state.chatUsers.map(user => {
                             return(
-                                <div className="chat-user online" key={user}>
+                                <div className={`chat-user ${this.state.userStatus}`} key={user.userId}>
+                                    <div className="display-name">{user.displayName}</div>
                                     <div className="user-thumb">
-                                        <img src={apiBaseUrl + '/file/global/image/male.png'} alt="User name" />
+                                        <img src={apiBaseUrl + user.profilePhoto} alt={user.displayName} />
                                     </div>
                                 </div>
                             )
@@ -27,21 +48,23 @@ export class ChatBar extends React.Component {
                     <div className="chatting-panel">
                         <div className="chat-box">
                             <div className="box-head">
-                                <div className="user-photo"><img src="" alt="sf"/></div>
+                                <div className="user-photo"><img src={apiBaseUrl + '/file/global/image/male.png'} alt="sf"/></div>
                                 <div className="display-name">Disaplay name</div>
+                                <div className="action">
+                                    <span className="close">x</span>
+                                </div>
                             </div>
                             <div className="box-body">
-                                <div className="message-item self">
+                                <div className="message-cell self">
                                     <span className="message">Message here</span>
-                                    <span className="actions">...</span>
+                                    <span className="action"><i className="icon-ellips-v"></i></span>
                                 </div>
-                                <div className="message-item other">
+                                <div className="message-cell other">
                                     <span className="message">Message here</span>
-                                    <span className="actions">...</span>
                                 </div>
                             </div>
                             <div className="box-form">
-                                <div className="emoji">emoji</div>
+                                <div className="emoji">:)</div>
                                 <div className="message-input">message input</div>
                                 <button className="send">=></button>
                             </div>
--- src/socket.js
+++ src/socket.js
@@ -2,3 +2,4 @@ import socketIOclient from "socket.io-client"
 import { loggedUserInfo } from "./services/commonService"
 
 export const socketConnection = socketIOclient(`http://localhost:8000?userId=${loggedUserInfo.userInfo.userId}`)
+
